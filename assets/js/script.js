cf98c61be59afc474573a96028c266c66daaaaec
jjuswwa <joshua.adarne0320@gmail.com>
2024-03-19 01:10:14 +0800

    section 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,51 @@
-var nav = document.querySelector("nav");
+// Navbar Scroll
+const navScrolled = document.querySelector(".navbar");
 
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 100) {
-    nav.classList.add("bg-dark", "shadow");
-  } else {
-    nav.classList.remove("bg-dark", "shadow");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 56) {
+    navScrolled.classList.add("navbar-scrolled");
+  } else if (window.screenY <= 56) {
+    navScrolled.classList.remove("navbar-scrolled");
   }
 });
 
-// Typewriter Effect per Word
-const words = ["Freedom.", "Rights.", "Case.", "Custody."];
-let i = 0;
-let timer;
+// Array of words to be typed
+const wordsToType = ["Freedom.", "Rights.", "Case.", "Custody."];
+let currentWordIndex = 0;
 
-function typeWriter() {
-  const span = document.querySelector(".typewriter-text");
-  if (i < words.length) {
-    span.textContent = ""; // Clear previous word
-    let currentWord = words[i];
-    let j = 0;
-    timer = setInterval(function () {
-      span.textContent += currentWord[j];
-      j++;
-      if (j === currentWord.length) {
-        clearInterval(timer);
-        i++;
-        setTimeout(typeWriter, 1000); // Pause before typing the next word
-      }
-    }, 100); // Typing speed per character
+// Function to type the text
+function typeWriter(text, i, speed) {
+  if (i < text.length) {
+    document.querySelector(".typewriter-text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(function () {
+      typeWriter(text, i, speed);
+    }, speed); // Adjust typing speed here
+  } else {
+    setTimeout(function () {
+      eraseText(text, i, speed);
+    }, 1000); // Wait for 1 second before erasing
   }
 }
 
-typeWriter();
+// Function to erase the text
+function eraseText(text, i, speed) {
+  if (i >= 0) {
+    var newText = text.substring(0, i);
+    document.querySelector(".typewriter-text").innerHTML = newText;
+    i--;
+    setTimeout(function () {
+      eraseText(text, i, speed);
+    }, speed); // Adjust erasing speed here
+  } else {
+    setTimeout(function () {
+      currentWordIndex = (currentWordIndex + 1) % wordsToType.length; // Move to the next word
+      typeWriter(wordsToType[currentWordIndex], 0, 200); // Adjust typing speed for next word
+    }, 500); // Wait for 0.5 second before typing next word
+  }
+}
+
+// Start typing effect with the first word
+typeWriter(wordsToType[currentWordIndex], 0, 300); // Adjust initial typing speed here
+
+// Mission & Vision //
